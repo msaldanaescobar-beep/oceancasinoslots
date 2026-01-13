963824c1004c02750faaa39793611e6bb9337623
msaldanaescobar-beep <m.saldana.escobar@gmail.com>
2026-01-13 19:57:03 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
 const style = document.createElement("style");
 style.innerHTML = `
 @keyframes pulse {
